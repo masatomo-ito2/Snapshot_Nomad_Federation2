2a031fb7b4a6100ac530285927e8280ab76bd9bd
masa3 <masa@hashicorp.com>
2020-10-07 12:13:28 +0900

    Initial

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  alias  = "japan"
+}
+
+data terraform_remote_state "this" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_org
+    workspaces = {
+      name = var.tfc_ws
+    }
+  }
+}
+
+locals {
+  public_subnets = data.terraform_remote_state.this.outputs.public_subnets_japan
+  vpc_id         = data.terraform_remote_state.this.outputs.vpc_id_japan
+}
+
+module "nomad-starter" {
+  providers = {
+    aws = aws.japan
+  }
+
+  source  = "hashicorp/nomad-starter/aws"
+  version = "0.2.1"
+  # insert the 6 required variables here
+
+	vpc_id = local.vpc_id
+	allowed_inbound_cidrs = var.allowed_inbound_cidrs
+	consul_version = var.consul_version
+	nomad_version = var.nomad_version
+	name_prefix = var.name_prefix
+	owner = var.owner
+	key_name = var.key_name
+
+	instance_type = var.instance_type
+	public_ip = var.public_ip
+}
+
---
+++ variables.tf
@@ -0,0 +1,46 @@
+# Required
+
+variable tfc_org {
+  default     = "xxxxxx"
+  description = "Org name that contains vpc settings"
+}
+
+variable tfc_ws {
+  default     = "xxxxxxxxx"
+  description = "Workspace name that contains vpc state"
+}
+
+variable key_name {
+  default     = "xxxxx"
+  description = "Keypair name to ssh"
+}
+
+variable owner  {
+	default = ""
+}
+
+variable consul_version {
+	default = "1.8.0"
+}
+
+variable nomad_version {
+	default = "0.12.5"
+}
+
+variable name_prefix {
+	default = ""
+}
+
+variable allowed_inboud_cidrs {
+	default = ["0.0.0.0/32"]
+}
+
+# Optional 
+
+variable instance_type = {
+	default = "t2.micro"
+}
+
+variable public_ip {
+	default = false
+}
